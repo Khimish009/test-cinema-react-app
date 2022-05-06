df9d20d31ac02e17d6eba897d426880bcf09e1aa
Salman <salman@salman.beeline>
2022-05-06 14:09:16 +0300

    feat: Improve movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,35 +4,38 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import { Link } from "react-router-dom";
 
 const MovieCard = () => {
   return (
     <Grid item>
-      <Card
-        sx={{
-          maxWidth: 240,
-          boxShadow: `8px 7px 5px -1px rgb(0 0 0 / 40%),
-          0px 1px 1px 0px rgb(0 0 0 / 11%),
-          -6px -1px 3px 0px rgb(0 0 0 / 14%)`
-        }}
-      >
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        sx={{
-          width: 240,
-        }}
-        image="https://kinopoiskapiunofficial.tech/images/posters/kp_small/435.jpg"
-      />
-      <CardContent sx={{ padding: '10px 5px !important', backgroundColor: '#1a191f' }}>
-        <Typography variant="subtitle1" component="div" sx={{ color: '#fff' }}>
-          Постучись в мою дверь
-        </Typography>
-        <Typography variant="subtitle2" color="yellow">
-          мелодрама, комедия
-        </Typography>
-      </CardContent>
-    </Card>
+      <Link style={{ textDecoration: 'none' }} to="/movies/1">
+        <Card
+          sx={{
+            maxWidth: 240,
+            boxShadow: `8px 7px 5px -1px rgb(0 0 0 / 40%),
+            0px 1px 1px 0px rgb(0 0 0 / 11%),
+            -6px -1px 3px 0px rgb(0 0 0 / 14%)`,
+          }}
+        >
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            sx={{
+              width: 240,
+            }}
+            image="https://kinopoiskapiunofficial.tech/images/posters/kp_small/435.jpg"
+          />
+          <CardContent sx={{ padding: '10px 5px !important', backgroundColor: '#1a191f' }}>
+            <Typography variant="subtitle1" component="div" sx={{ color: '#fff', textDecoration: 'none' }}>
+              Постучись в мою дверь
+            </Typography>
+            <Typography variant="subtitle2" color="yellow">
+              мелодрама, комедия
+            </Typography>
+          </CardContent>
+        </Card>
+      </Link>
     </Grid>
   );
 }
